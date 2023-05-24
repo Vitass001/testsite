ba6471166e3ab04cd8f1f1b89125d84be2a5d18b
User <roman.victor2015@gmail.com>
2023-05-24 12:07:01 +0300

    TEST-SITE5.0

--- testsite/testsite/settings.py
+++ testsite/testsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-47dj6hi4h4i_pl_ba8!onszbzdv9#lm9-sm9-4z7e@96mpti8o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
