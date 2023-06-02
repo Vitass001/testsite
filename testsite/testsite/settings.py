aef4bbfa00e7cdbe9f1758787a4483996b935fe9
User <roman.victor2015@gmail.com>
2023-06-02 11:58:38 +0300

    TEST-SITE5.0

--- testsite/testsite/settings.py
+++ testsite/testsite/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -134,6 +135,6 @@ MEDIA_URL = '/media/'
 
 
 
-STATIC_ROOT = '/static/'
+
 FILE_UPLOAD_HANDLERS = ["django.core.files.uploadhandler.MemoryFileUploadHandler", "django.core.files.uploadhandler.TemporaryFileUploadHandler"]
 
